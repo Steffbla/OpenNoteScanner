f737af1ee46ff774ba963b16a68e1c14714209ee
Stefan Teichner <stefan.teichner@stud.uni-regensburg.de>
2020-04-08 09:12:49 +0200

    Refactor ImageSwipeAdapter

--- app/src/main/java/com/todobom/opennotescanner/helpers/ImageSwipeAdapter.java
+++ app/src/main/java/com/todobom/opennotescanner/helpers/ImageSwipeAdapter.java
@@ -18,31 +18,27 @@ import java.util.ArrayList;
 public class ImageSwipeAdapter extends RecyclerView.Adapter<ImageSwipeAdapter.ImageViewHolder> {
 
     private static final String TAG = "ImageSwipeAdapter";
-    OnImageSwipeListener listener;
-    int reversePos;
+    private OnImageSwipeListener listener;
     private ArrayList<String> fileUris;
 
     public ImageSwipeAdapter(OnImageSwipeListener listener, ArrayList<String> fileUris) {
         Log.d(TAG, "ImageSwipeAdapter: init");
         this.listener = listener;
         this.fileUris = fileUris;
-//        Collections.reverse(this.fileUris);
     }
 
     @NonNull
     @Override
     public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         Log.d(TAG, "onCreateViewHolder: ");
-//        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.image_page, parent, false);
         return new ImageViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.image_page, parent,
                 false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
-        reversePos = getItemCount() - position - 1;
+        int reversePos = getItemCount() - position - 1;
         holder.setImageView(fileUris.get(reversePos));
-//        listener.onSwipeImage(reversePos);
     }
 
     @Override
@@ -57,16 +53,16 @@ public class ImageSwipeAdapter extends RecyclerView.Adapter<ImageSwipeAdapter.Im
     }
 
 
-    class ImageViewHolder extends RecyclerView.ViewHolder {
+    static class ImageViewHolder extends RecyclerView.ViewHolder {
 
-        ImageView imageView;
+        private ImageView imageView;
 
-        public ImageViewHolder(@NonNull View itemView) {
+        ImageViewHolder(@NonNull View itemView) {
             super(itemView);
             imageView = itemView.findViewById(R.id.preview_image);
         }
 
-        public void setImageView(String fileUri) {
+        void setImageView(String fileUri) {
             imageView.setImageURI(Uri.parse(fileUri));
         }
     }
